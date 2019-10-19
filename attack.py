22fe31282802e6326bbe7952b3dbdc43a1259d7b
Théo Guidoux <theoguidoux@gmail.com>
2019-10-19 16:59:03 +0200

    main file for attack

---
+++ attack.py
@@ -0,0 +1,13 @@
+"""
+File: anonymizer.py
+Author: Cat1 Narvali
+Email: cat1narvali@gmail.com
+Github: https://github.com/TheRaccoon00/anonymization
+Description: Database anonymizer for DARC competition
+"""
+
+def main():
+    #main
+
+if __name__ == "__main__":
+    main()
