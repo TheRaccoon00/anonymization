df61bf6c0e7c3d3ae6236593fa9bfcd128fbe691
Théo Guidoux <theoguidoux@gmail.com>
2019-11-04 19:08:45 +0100

    search for groups + choice of what to vectorize + choice to use scaler

--- attack.py
+++ attack.py
@@ -9,13 +9,20 @@ Description: Database anonymizer for DARC competition
 import pandas as pd
 import time, random
 import numpy as np
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, RobustScaler, MaxAbsScaler
 from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
 
 #turnn off fucking ugly warnings
 import warnings
 from sklearn.exceptions import DataConversionWarning
 warnings.filterwarnings(action='ignore', category=DataConversionWarning)
+
+
+date_index = 0
+hours_index = -1
+item_index = 1
+
+use_scaler = False
 
 def vectorize(X, num_col):
 	"""
@@ -44,20 +51,31 @@ def vectorize(X, num_col):
 def learn_panda_to_vector(df, scaler):
 	X = np.asarray(df)
 
+	trans_table_date = dict()
+	trans_table_date_rev = dict()
+	trans_table_hours = dict()
+	trans_table_hours_rev = dict()
+	trans_table_item = dict()
+	trans_table_item_rev = dict()
+
 	#apply vectorization
-	print("Transforming date")
-	trans_table_date, trans_table_date_rev, X = vectorize(X, 1)
+	if date_index != -1:
+		print("Transforming date")
+		trans_table_date, trans_table_date_rev, X = vectorize(X, date_index)
 
-	print("Transforming hours")
-	trans_table_hours, trans_table_hours_rev, X = vectorize(X, 2)
+	if hours_index != -1:
+		print("Transforming hours")
+		trans_table_hours, trans_table_hours_rev, X = vectorize(X, hours_index)
 
-	print("Transforming id_item")
-	trans_table_item, trans_table_item_rev, X = vectorize(X, 3)
+	if item_index != -1:
+		print("Transforming id_item")
+		trans_table_item, trans_table_item_rev, X = vectorize(X, item_index)
 
 	#print(X[0]) => [17850 0 10 241 2.55 6]
 
 	#apply scaler to input
-	X = scaler.fit_transform(X)
+	if use_scaler:
+		X = scaler.fit_transform(X)
 	return trans_table_date, trans_table_date_rev, trans_table_hours, trans_table_hours_rev, trans_table_item, trans_table_item_rev, X
 
 def apply_transform(dfn, trans_table, num_col):
@@ -102,16 +120,39 @@ def get_similar(Xgt, Xdt_row, return_length=10):
 	similar_rows = []
 	similar_rows_score = []
 	for i in range(0, return_length):
-		similar_rows.append(Xgt[distances[i][0]])
+		similar_rows.append((distances[i][0], Xgt[distances[i][0]]))
 		similar_rows_score.append(distances[i][1])
 
 	return np.asarray(similar_rows), similar_rows_score
 
+def data_to_vector(data, scaler, trans_table_date, trans_table_hours, trans_table_item):
+	dtn = np.asarray([data])
+	if date_index != -1:
+		dtn, _, _ = apply_transform(dtn, trans_table_date, date_index)
+
+	if hours_index != -1:
+		dtn, _, _ = apply_transform(dtn, trans_table_hours, hours_index)
+
+	if item_index != -1:
+		dtn, _, _ = apply_transform(dtn, trans_table_item, item_index)
+
+	transformed = dtn
+	if use_scaler:
+		transformed = scaler.transform(dtn)
+	return transformed[0]
+
 def reverse_vector(vec, scaler, trans_table_date_rev, trans_table_hours_rev, trans_table_item_rev):
-	unscaled_vec = list(scaler.inverse_transform([vec])[0])
-	unscaled_vec[1] = trans_table_date_rev[int(unscaled_vec[1])]
-	unscaled_vec[2] = trans_table_hours_rev[int(unscaled_vec[2])]
-	unscaled_vec[3] = trans_table_item_rev[int(unscaled_vec[3])]
+	if use_scaler:
+		unscaled_vec = list(scaler.inverse_transform([vec])[0])
+	else:
+		unscaled_vec = vec
+	if date_index != -1:
+		unscaled_vec[date_index] = trans_table_date_rev[int(unscaled_vec[date_index])]
+	if hours_index != -1:
+		unscaled_vec[hours_index] = trans_table_hours_rev[int(unscaled_vec[hours_index])]
+	if item_index != -1:
+		unscaled_vec[item_index] = trans_table_item_rev[int(unscaled_vec[item_index])]
+
 	return unscaled_vec
 
 def main():
@@ -119,56 +160,104 @@ def main():
 	#load gt and dt is the anonymized dataset
 	gt = pd.read_csv("ground_truth.csv", sep=",")
 	#works for S_Godille_Table_1, S_Godille_Table_2, S_Godille_Table_3
-	dt = pd.read_csv("/home/theoguidoux/INSA/ws/projetsecu4a/docs/CSV_RENDU/S_Godille_Table_1.csv", sep=",")
+	dt = pd.read_csv("/home/theoguidoux/INSA/ws/projetsecu4a/docs/CSV_RENDU/S_Godille_Table_3.csv", sep=",")
 
 	print("Converting datasets...")
 	#convert it to numpy array to speed up things
 	gtn = np.asarray(gt)
 	dtn = np.asarray(dt)
+	#[17850 '2010/12/01' '08:26' '85123A' 2.55 6]
+
+	#delete some axes
+	gtn = np.delete(gtn, [0, 2], axis=1)
+	dtn = np.delete(dtn, [0, 2], axis=1)
 
 	#shapes
 	#print(gtn.shape) #(307054, 6)
 	#print(dtn.shape) #(314024, 6)
 
 	#mega scaler of the death
-	sscaler = StandardScaler()
-
+	#sscaler = StandardScaler()
+	sscaler = MaxAbsScaler()
 	#print(gtn[0]) #[17850 '2010/12/01' '08:26' '85123A' 2.55 6]
 	#print(dtn[0]) #[830706 '2011/11/01' '00:00' '23219' 2.001 1]
 
 	print("Learning transformation from ground_truth and scalarizing inputs...")
 	#learn how to convert str items to int items and store it in tables for date/hour/item
-	trans_table_date, trans_table_date_rev, trans_table_hours, trans_table_hours_rev, trans_table_item, trans_table_item_rev, Xgt = learn_panda_to_vector(gt, sscaler)
+	trans_table_date, trans_table_date_rev, trans_table_hours, trans_table_hours_rev, trans_table_item, trans_table_item_rev, Xgt = learn_panda_to_vector(gtn, sscaler)
 	#print(Xgt.shape) #(307054, 6)
 	#print(Xgt[0]) #[ 1.46934958 -0.83609854 -0.35887791  0.47985661 -0.02553646 -0.15785763]
 
 	print("Applying transformation to anonymized dataset...")
 	#now apply learnt vectorization on anonymized data
-	dtn, trans_table_date, trans_table_date_rev = apply_transform(dtn, trans_table_date, 1)
-	dtn, trans_table_hours, trans_table_hours_rev = apply_transform(dtn, trans_table_hours, 2)
-	dtn, trans_table_item, trans_table_item_rev = apply_transform(dtn, trans_table_item, 3)
+	dtn_transformed = dtn.copy()
+	if date_index != -1:
+		dtn_transformed, trans_table_date, trans_table_date_rev = apply_transform(dtn_transformed, trans_table_date, date_index)
+
+	if hours_index != -1:
+		dtn_transformed, trans_table_hours, trans_table_hours_rev = apply_transform(dtn_transformed, trans_table_hours, hours_index)
+
+	if item_index != -1:
+		dtn_transformed, trans_table_item, trans_table_item_rev = apply_transform(dtn_transformed, trans_table_item, item_index)
+
 
 	print("Applying scalarization to anonymized dataset...")
-	Xdt = sscaler.transform(dtn)
-
+	if use_scaler:
+		Xdt = sscaler.transform(dtn_transformed)
+	else:
+		Xdt = dtn_transformed
 	#print(Xdt.shape) #(314024, 6)
 	#print(Xdt[0]) #[ 4.71031071e+02 -1.26383084e-01  2.05117891e+00 -1.25426197e+00 -4.89814730e-02 -2.76827083e-01]
 
-	print("Getting similar vector ...")
-	#to test : change dtn[0] to gtn[0] and Xdt[0] to Xgt[0]
+#	print("Getting similar vector ...")
+#	#to test : change dtn[0] to gtn[0] and Xdt[0] to Xgt[0]
+#
+#	rl = 10
+#	vec_index = 0
+#
+#	print(gtn[vec_index])
+#	print(dtn[vec_index])
+#	sim_vectors, sim_scores = get_similar(Xgt, Xdt[vec_index], return_length=rl)
+#	for i in range(0, rl):
+#		sim_vec = sim_vectors[i]
+#		sim_score = sim_scores[i]
+#		rev_sim_vec = reverse_vector(sim_vec, sscaler, trans_table_date_rev, trans_table_hours_rev, trans_table_item_rev)
+#		print("(d = "+str(sim_score)+")", rev_sim_vec)
 
-	rl = 10
-	vec_index = 0
+	print("Getting similar vectors ...")
+	nb_result = 1 #change this to have more result
+	result = []
+	for i in range(0, 5):#dtn.shape[0]
+		print("Computing "+str(i+1)+" out of "+str(Xdt.shape[0]), end="\r")
+		##print(str(round((i*100)/Xdt.shape[0], 3))+"%", end="\r")
+		input_data = dtn[i]
+		input_vec = data_to_vector(input_data, sscaler, trans_table_date, trans_table_hours, trans_table_item)
+		#find the closest vector to Xdt[i] in Xgt
+		sim_vectors, sim_scores = get_similar(Xgt, input_vec, return_length=nb_result)
+		result.append((i, input_data.tolist(), sim_vectors, sim_scores))
 
-	print(gtn[vec_index])
-	print(dtn[vec_index])
-	sim_vectors, sim_scores = get_similar(Xgt, Xdt[vec_index], return_length=rl)
-	for i in range(0, rl):
-		sim_vec = sim_vectors[i]
-		sim_score = sim_scores[i]
-		rev_sim_vec = reverse_vector(sim_vec, sscaler, trans_table_date_rev, trans_table_hours_rev, trans_table_item_rev)
-		print("(d = "+str(sim_score)+")", rev_sim_vec)
-
+	#print result
+	print("\n\n\n")
+	print("Results :")
+	for res in result:
+		anonymized_entry_index = res[0]
+		anonymized_entry = res[1]
+		closest_vecs = res[2]
+		scores = res[3]
+		for i in range(0, len(closest_vecs)):
+			init_closest_vec_index = closest_vecs[i][0]
+			closest_vec = closest_vecs[i][1]
+			score = scores[i]
+			init_closest_vec = np.asarray(gt.loc[init_closest_vec_index])
+			init_anonymized_vec = np.asarray(dt.loc[anonymized_entry_index])
+			closest_entry = reverse_vector(closest_vec, sscaler, trans_table_date_rev, trans_table_hours_rev, trans_table_item_rev)
+			print("#"*50)
+			print("data :", init_anonymized_vec, "=>", init_closest_vec)
+			print("vectors :", anonymized_entry, "=>", closest_entry)
+			print("score =", score, " == ? :", anonymized_entry == closest_entry)
 
 if __name__ == "__main__":
-    main()
+	print("################################################")
+	print("## TODO : vecotrize date, meaning that '2011/12/01' => 2011, 12, 01, i thing it gonna increase accuracy since scalarized date have nothing in common")
+	print("################################################")
+	main()
