56a8be96a3cd15c55cc729a44a1aeec191f4a262
Théo Guidoux <theoguidoux@gmail.com>
2019-11-13 20:45:36 +0100

    split dates to have better performance

--- attack.py
+++ attack.py
@@ -17,11 +17,15 @@ import warnings
 from sklearn.exceptions import DataConversionWarning
 warnings.filterwarnings(action='ignore', category=DataConversionWarning)
 
+#best values :
+#date_index = 0
+#hours_index = -1
+#item_index = 1
+#use_scaler = False
 
 date_index = 0
 hours_index = -1
 item_index = 1
-
 use_scaler = False
 
 def vectorize(X, num_col):
@@ -51,8 +55,12 @@ def vectorize(X, num_col):
 def learn_panda_to_vector(df, scaler):
 	X = np.asarray(df)
 
-	trans_table_date = dict()
-	trans_table_date_rev = dict()
+	trans_table_date_y = dict()
+	trans_table_date_y_rev = dict()
+	trans_table_date_m = dict()
+	trans_table_date_m_rev = dict()
+	trans_table_date_d = dict()
+	trans_table_date_d_rev = dict()
 	trans_table_hours = dict()
 	trans_table_hours_rev = dict()
 	trans_table_item = dict()
@@ -61,7 +69,10 @@ def learn_panda_to_vector(df, scaler):
 	#apply vectorization
 	if date_index != -1:
 		print("Transforming date")
-		trans_table_date, trans_table_date_rev, X = vectorize(X, date_index)
+		trans_table_date_y, trans_table_date_y_rev, X = vectorize(X, date_index)
+		trans_table_date_m, trans_table_date_m_rev, X = vectorize(X, date_index+1)
+		trans_table_date_d, trans_table_date_d_rev, X = vectorize(X, date_index+2)
+
 
 	if hours_index != -1:
 		print("Transforming hours")
@@ -76,7 +87,10 @@ def learn_panda_to_vector(df, scaler):
 	#apply scaler to input
 	if use_scaler:
 		X = scaler.fit_transform(X)
-	return trans_table_date, trans_table_date_rev, trans_table_hours, trans_table_hours_rev, trans_table_item, trans_table_item_rev, X
+	return 	trans_table_date_y, trans_table_date_y_rev,\
+			trans_table_date_m, trans_table_date_m_rev,\
+			trans_table_date_d, trans_table_date_d_rev,\
+			trans_table_hours, trans_table_hours_rev, trans_table_item, trans_table_item_rev, X
 
 def apply_transform(dfn, trans_table, num_col):
 	"""
@@ -125,10 +139,12 @@ def get_similar(Xgt, Xdt_row, return_length=10):
 
 	return np.asarray(similar_rows), similar_rows_score
 
-def data_to_vector(data, scaler, trans_table_date, trans_table_hours, trans_table_item):
+def data_to_vector(data, scaler, trans_table_date_y, trans_table_date_m, trans_table_date_d, trans_table_hours, trans_table_item):
 	dtn = np.asarray([data])
 	if date_index != -1:
-		dtn, _, _ = apply_transform(dtn, trans_table_date, date_index)
+		dtn, _, _ = apply_transform(dtn, trans_table_date_y, date_index)
+		dtn, _, _ = apply_transform(dtn, trans_table_date_m, date_index+1)
+		dtn, _, _ = apply_transform(dtn, trans_table_date_d, date_index+2)
 
 	if hours_index != -1:
 		dtn, _, _ = apply_transform(dtn, trans_table_hours, hours_index)
@@ -141,13 +157,16 @@ def data_to_vector(data, scaler, trans_table_date, trans_table_hours, trans_tabl
 		transformed = scaler.transform(dtn)
 	return transformed[0]
 
-def reverse_vector(vec, scaler, trans_table_date_rev, trans_table_hours_rev, trans_table_item_rev):
+def reverse_vector(vec, scaler, trans_table_date_y_rev, trans_table_date_m_rev, trans_table_date_d_rev, trans_table_hours_rev, trans_table_item_rev):
 	if use_scaler:
 		unscaled_vec = list(scaler.inverse_transform([vec])[0])
 	else:
 		unscaled_vec = vec
 	if date_index != -1:
-		unscaled_vec[date_index] = trans_table_date_rev[int(unscaled_vec[date_index])]
+		unscaled_vec[date_index] = trans_table_date_y_rev[int(unscaled_vec[date_index])]
+		unscaled_vec[date_index+1] = trans_table_date_m_rev[int(unscaled_vec[date_index+1])]
+		unscaled_vec[date_index+2] = trans_table_date_d_rev[int(unscaled_vec[date_index+2])]
+
 	if hours_index != -1:
 		unscaled_vec[hours_index] = trans_table_hours_rev[int(unscaled_vec[hours_index])]
 	if item_index != -1:
@@ -155,7 +174,18 @@ def reverse_vector(vec, scaler, trans_table_date_rev, trans_table_hours_rev, tra
 
 	return unscaled_vec
 
+def split_date(X, date_index):
+	out = []
+	for i in range(0, X.shape[0]):
+		y, m, d = X[i][date_index].split("/")
+		y, m, d = int(y), int(m), int(d)
+		x_i = list(X[i])
+		x_i[date_index:date_index+1] = [y, m, d]
+		out.append(x_i)
+	return np.asarray(out)
+
 def main():
+	global date_index, item_index, hours_index, use_scaler
 	print("Reading datasets...")
 	#load gt and dt is the anonymized dataset
 	gt = pd.read_csv("ground_truth.csv", sep=",")
@@ -168,9 +198,17 @@ def main():
 	dtn = np.asarray(dt)
 	#[17850 '2010/12/01' '08:26' '85123A' 2.55 6]
 
-	#delete some axes
+	#delete some axes id and hours
 	gtn = np.delete(gtn, [0, 2], axis=1)
 	dtn = np.delete(dtn, [0, 2], axis=1)
+
+
+	if date_index != -1:
+		print("Splitting dates...")
+		#split date
+		gtn = split_date(gtn, date_index)
+		dtn = split_date(dtn, date_index)
+		item_index = item_index + 2 #deleted 1 item, added 3 so add 2 to item_index
 
 	#shapes
 	#print(gtn.shape) #(307054, 6)
@@ -184,7 +222,14 @@ def main():
 
 	print("Learning transformation from ground_truth and scalarizing inputs...")
 	#learn how to convert str items to int items and store it in tables for date/hour/item
-	trans_table_date, trans_table_date_rev, trans_table_hours, trans_table_hours_rev, trans_table_item, trans_table_item_rev, Xgt = learn_panda_to_vector(gtn, sscaler)
+
+	trans_table_date_y, trans_table_date_y_rev,\
+	trans_table_date_m,trans_table_date_m_rev,\
+	trans_table_date_d, trans_table_date_d_rev,\
+	trans_table_hours, trans_table_hours_rev,\
+	trans_table_item, trans_table_item_rev,\
+	Xgt = learn_panda_to_vector(gtn, sscaler)
+
 	#print(Xgt.shape) #(307054, 6)
 	#print(Xgt[0]) #[ 1.46934958 -0.83609854 -0.35887791  0.47985661 -0.02553646 -0.15785763]
 
@@ -192,7 +237,9 @@ def main():
 	#now apply learnt vectorization on anonymized data
 	dtn_transformed = dtn.copy()
 	if date_index != -1:
-		dtn_transformed, trans_table_date, trans_table_date_rev = apply_transform(dtn_transformed, trans_table_date, date_index)
+		dtn_transformed, trans_table_date_y, trans_table_date_y_rev = apply_transform(dtn_transformed, trans_table_date_y, date_index)
+		dtn_transformed, trans_table_date_m, trans_table_date_m_rev = apply_transform(dtn_transformed, trans_table_date_m, date_index+1)
+		dtn_transformed, trans_table_date_d, trans_table_date_d_rev = apply_transform(dtn_transformed, trans_table_date_d, date_index+2)
 
 	if hours_index != -1:
 		dtn_transformed, trans_table_hours, trans_table_hours_rev = apply_transform(dtn_transformed, trans_table_hours, hours_index)
@@ -206,6 +253,10 @@ def main():
 		Xdt = sscaler.transform(dtn_transformed)
 	else:
 		Xdt = dtn_transformed
+	Xgt = np.asarray(Xgt, dtype=np.float64)
+	Xdt = np.asarray(Xdt, dtype=np.float64)
+	print(Xgt[0])
+	print(Xdt[0])
 	#print(Xdt.shape) #(314024, 6)
 	#print(Xdt[0]) #[ 4.71031071e+02 -1.26383084e-01  2.05117891e+00 -1.25426197e+00 -4.89814730e-02 -2.76827083e-01]
 
@@ -231,7 +282,7 @@ def main():
 		print("Computing "+str(i+1)+" out of "+str(Xdt.shape[0]), end="\r")
 		##print(str(round((i*100)/Xdt.shape[0], 3))+"%", end="\r")
 		input_data = dtn[i]
-		input_vec = data_to_vector(input_data, sscaler, trans_table_date, trans_table_hours, trans_table_item)
+		input_vec = data_to_vector(input_data, sscaler, trans_table_date_y, trans_table_date_m, trans_table_date_d, trans_table_hours, trans_table_item)
 		#find the closest vector to Xdt[i] in Xgt
 		sim_vectors, sim_scores = get_similar(Xgt, input_vec, return_length=nb_result)
 		result.append((i, input_data.tolist(), sim_vectors, sim_scores))
@@ -250,7 +301,7 @@ def main():
 			score = scores[i]
 			init_closest_vec = np.asarray(gt.loc[init_closest_vec_index])
 			init_anonymized_vec = np.asarray(dt.loc[anonymized_entry_index])
-			closest_entry = reverse_vector(closest_vec, sscaler, trans_table_date_rev, trans_table_hours_rev, trans_table_item_rev)
+			closest_entry = reverse_vector(closest_vec, sscaler, trans_table_date_y_rev, trans_table_date_m_rev, trans_table_date_d_rev, trans_table_hours_rev, trans_table_item_rev)
 			print("#"*50)
 			print("data :", init_anonymized_vec, "=>", init_closest_vec)
 			print("vectors :", anonymized_entry, "=>", closest_entry)
@@ -258,6 +309,6 @@ def main():
 
 if __name__ == "__main__":
 	print("################################################")
-	print("## TODO : vecotrize date, meaning that '2011/12/01' => 2011, 12, 01, i thing it gonna increase accuracy since scalarized date have nothing in common")
+	print("## Add similarity rules like quantity exactitude cause qtty may not be anonymized")
 	print("################################################")
 	main()
