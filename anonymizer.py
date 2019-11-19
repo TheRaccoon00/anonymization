47b65ba9bedcbbb5c374e1e21214621f013bf4b2
cfoissard <cfoissard@CFOISSARD-UX.home>
2019-11-19 22:27:42 +0100

    modif

--- anonymizer.py
+++ anonymizer.py
@@ -8,32 +8,33 @@ Description: Database anonymizer for DARC competition
 
 import pandas as pd
 import numpy as np
-import random
 from pandas import DataFrame, Series
+import hashlib
 
 def main():
     #main
-    pseudo()
+    pseudo('ground_truth.csv')
     #main
 
-def pseudo():
-    df =  pd.read_csv('ground_truth.csv',dtype={"id_user":np.int,"date":np.object,"hours":np.object,"id_item":np.object,"price":np.float,"qty":np.int})
-    df.head()
-    df.info()
-    size = len(df["id_user"])
+def pseudo(filename):
+    #Import CSV file
+    df =  pd.read_csv(filename,dtype={"id_user":np.int,"date":np.object,"hours":np.object,"id_item":np.object,"price":np.float,"qty":np.int})
     id_user_set = set(df["id_user"])
-    print(len(id_user_set))
     trans_table = dict()
+
+    #Create new id
     for new_id, id in enumerate(id_user_set):
         trans_table[id] = new_id
-    print(df["id_user"][0], trans_table[df["id_user"][0]])
     dfn = np.asarray(df)
-    for i in range(0,size):
-        dfn[i][0] = trans_table[df["id_user"][i]]
+
+    #Hash and store new id
+    for i in range(0,len(df["id_user"])):
+        dfn[i][0] = hashlib.sha512((str(trans_table[df["id_user"][i]])+"Il utilise plutôt un système de refroidissement par évaporation avancé qui tire les eaux grises d'un canal industriel situé à proximité.").encode()).hexdigest()
+
+    #Create new CSV
     df = pd.DataFrame(data=np.asarray(dfn))
     df.columns = ["id_user", "date", "hours", "id_item","price","qty"]
-    df.to_csv('submission.csv', encoding='utf-8',index=False)
-    print(df["id_user"])
+    df.to_csv('stage1.csv', encoding='utf-8',index=False)
 
 if __name__ == "__main__":
     main()
