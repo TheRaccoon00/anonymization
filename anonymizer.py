46bdccc2978d5bad4c992a7c7c1123628109e6ef
cfoissard <cfoissard@CFOISSARD-UX.home>
2019-11-19 23:05:43 +0100

    basic functions

--- anonymizer.py
+++ anonymizer.py
@@ -14,6 +14,8 @@ import hashlib
 def main():
     #main
     pseudo('ground_truth.csv')
+    modificateDate('out/stage1.csv')
+    del_hours('out/stage2.csv')
     #main
 
 def pseudo(filename):
@@ -29,12 +31,34 @@ def pseudo(filename):
 
     #Hash and store new id
     for i in range(0,len(df["id_user"])):
-        dfn[i][0] = hashlib.sha512((str(trans_table[df["id_user"][i]])+"Il utilise plutôt un système de refroidissement par évaporation avancé qui tire les eaux grises d'un canal industriel situé à proximité.").encode()).hexdigest()
-
+        #dfn[i][0] = int(hashlib.md5((str(trans_table[df["id_user"][i]])+"Il utilise plutôt un système de refroidissement par évaporation avancé qui tire les eaux grises d'un canal industriel situé à proximité.").encode()).hexdigest(),16)
+        dfn[i][0] = trans_table[df["id_user"][i]]
     #Create new CSV
     df = pd.DataFrame(data=np.asarray(dfn))
     df.columns = ["id_user", "date", "hours", "id_item","price","qty"]
-    df.to_csv('stage1.csv', encoding='utf-8',index=False)
+    df.to_csv('out/stage1.csv', encoding='utf-8',index=False)
+
+def modificateDate(filename):
+    df =  pd.read_csv(filename,dtype={"id_user":np.int,"date":np.object,"hours":np.object,"id_item":np.object,"price":np.float,"qty":np.int})
+    size = len(df["date"])
+    listDate = []
+    for i in range(size):
+        listDate.append(df["date"][i])
+
+    dfn = np.asarray(df)
+
+    for i in range(size):
+        dfn[i][1] = listDate[i][:4] + "/" + listDate[i][5:7] + "/" + "01" # 1 is for date
+
+    df = pd.DataFrame(data=np.asarray(dfn))
+    df.columns = ["id_user", "date", "hours", "id_item","price","qty"]
+    df.to_csv('out/stage2.csv', encoding='utf-8',index=False)
+
+def del_hours(filename):
+    df =  pd.read_csv(filename,dtype={"id_user":np.int,"date":np.object,"hours":np.object,"id_item":np.object,"price":np.float,"qty":np.int})
+    for col in df.columns:
+        df['hours'].values[:] = 'DEL'
+    df.to_csv('out/stage3.csv',encoding='utf-8',index=False)
 
 if __name__ == "__main__":
     main()
